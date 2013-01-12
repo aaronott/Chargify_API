f71b8935c60ebb1fcda1c87f472cb4992f3faa04
Aaron Ott <aaron.ott@gmail.com>
2013-01-11 22:03:27 -0700

    Clean CommonTest

--- Tests/CommonTest.php
+++ Tests/CommonTest.php
@@ -14,47 +14,47 @@ require_once 'Chargify/Customer.php';
 
 class Chargify_CommonTest extends PHPUnit_Framework_TestCase
 {
-  
+
   protected $Customer;
-  
+
   public function setUp()
   {
     $this->Customer = Chargify::factory('Customer');
   }
-  
+
   /**
-	 * Tests Connect
-	 *
-	 * Tests connection by sending through a call
-	 *
-	 * @test
-	 */
+   * Tests Connect
+   *
+   * Tests connection by sending through a call
+   *
+   * @test
+   */
   public function testConnect()
   {
     $this->Customer->all();
     $connect = $this->Customer->callInfo;
-     
+
     // test a simple connect
     $this->assertTrue($connect['http_code'] === 200);
   }
-  
+
   /**
-	 * Tests Connect
-	 *
-	 * Tests connection by sending through a call with an invalid key
-	 *
-	 * @test
-	 */
+   * Tests Connect
+   *
+   * Tests connection by sending through a call with an invalid key
+   *
+   * @test
+   */
   public function testFailedConnect()
   {
     $apikey = Chargify::$apikey;
     Chargify::$apikey .= 'x';
     $this->Customer->all();
     $connect = $this->Customer->callInfo;
-    
+
     // test a simple connect
     $this->assertTrue($connect['http_code'] === 401);
-    
+
     Chargify::$apikey = $apikey;
   }
 }
